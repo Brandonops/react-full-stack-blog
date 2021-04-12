daf7ecaf1938ac69925da7690ec575f2d33fe2ac
Brandon <woolstonbj11@gmail.com>
2021-04-12 17:54:28 -0400

    fixed errors in lesson

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const models = require('../models');
 const checkAuth = require('../auth/checkAuth')
 
-/* GET home page. */
+/* GET posts. */
 router.get('/', async (req, res) => {
     const posts = await models.Post.findAll({
       include: [{ model: models.User, attributes: ['username', 'id'] }],
@@ -11,29 +11,63 @@ router.get('/', async (req, res) => {
   
     res.json(posts);
   });
-router.post('/', checkAuth, async (req, res) => {
-    const { user } = req.session
-    if (!user) {
-        return res.status(401).json({
-            error: "Not logged in"
-        });
-    };
-
+  
+  router.post('/', checkAuth, async (req, res) => {
+    // if any fields missing
     if (!req.body.title || !req.body.content) {
-        return req.status(400).json({
-            error: 'Please include all title and content fields'
-        });
-    };
-    //create post 
+      //   send 400 error
+      return req.status(400).json({
+        error: 'Please include all title and content',
+      });
+    }
+  
+    // create new post
     const post = await models.Post.create({
-        title: req.body.title,
-        content: req.body.content,
-        UserId: req.session.user.id
+      title: req.body.title,
+      content: req.body.content,
+      UserId: req.session.user.id,
     });
-    
-
-    "send back new post data"
+  
+    // send back new post data
     res.status(201).json(post);
-});
-
-module.exports = router;
+  });
+  
+  router.post('/:id/comments', checkAuth, async (req, res) => {
+    const post = await models.Post.findByPk(req.params.id);
+    if (!post) {
+      res.status(404).json({
+        error: 'Could not find post with that id',
+      });
+    }
+  
+    if (!req.body.text) {
+      res.status(400).json({
+        error: 'please include all required fields',
+      });
+    }
+  
+    const comment = await post.createComment({
+      text: req.body.text,
+      PostId: req.params.id,
+      UserId: req.session.user.id,
+    });
+  
+    res.status(201).json(comment);
+  });
+  
+  router.get('/:id/comments', async (req, res) => {
+    const post = await models.Post.findByPk(req.params.id);
+    if (!post) {
+      res.status(404).json({
+        error: 'Could not find post with that id',
+      });
+    }
+  
+    const comments = await post.getComments({
+      include: [{ model: models.User, attributes: ['username', 'id'] }],
+    });
+  
+    res.json(comments);
+  });
+  
+  module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,116 +4,113 @@ const models = require('../models')
 const bcrypt = require('bcrypt')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-//POST localhost:3000/api/v1/users
+// POST /api/v1/users/register
 router.post('/register', async (req, res) => {
-  if(!req.body.username || !req.body.password) {
-    return res.status(401).json({
-      error: "Please include username and password"
-    })
+  // check for username and password on request
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({
+      error: 'Please include username and password',
+    });
   }
-//check db for existing user
- const user = await models.User.findOne({
-    where: {
-      username: req.body.username
-    }
-  })
 
-  //if exists, send error
+  // check database for existing user
+  const user = await models.User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  });
+
+  // if exists, send error
   if (user) {
     return res.status(400).json({
-      error: 'Username already in use'
-    })
+      error: 'Username already in use',
+    });
   }
 
-  //create password
-  const hash = await bcrypt.hash(req.body.password, 10)
+  // hash password
+  const hash = await bcrypt.hash(req.body.password, 10);
 
-  //create user
+  // create user
   const newUser = await models.User.create({
     username: req.body.username,
     password: hash,
-  })
+  });
 
-  return res.status(201).json(newUser)
-})
-
+  // respond with success message
+  return res.status(201).json({});
+});
 
 router.post('/login', async (req, res) => {
-  if(!req.body.username || !req.body.password) {
-    return res.status(401).json({
-      error: "Please include username and password"
-    })
+  // check for username password
+  // if not there, send error
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({
+      error: 'Please include username and password',
+    });
   }
 
-//check db for existing user
- const user = await models.User.findOne({
+  // find user from username
+  const user = await models.User.findOne({
     where: {
-      username: req.body.username
-    }
-  })
+      username: req.body.username,
+    },
+  });
 
-   //if exists, send error
-   if (!user) {
+  // if no user, send error
+  if (!user) {
     return res.status(404).json({
-      error: 'No user with that username found'
-    })
+      error: 'No user with that username found',
+    });
   }
 
-  //check password
-  const match = await bcrypt.compare(req.body.password, user.password)
+  // check password
+  const match = await bcrypt.compare(req.body.password, user.password);
 
+  // if no match, send error
   if (!match) {
     return res.status(401).json({
-      error: 'Password incorrect'
-    })
+      error: 'Password incorrect',
+    });
   }
 
-
-  //store user in session
+  // store user in session
   req.session.user = user;
 
-  //respond with user info
-  // res.json(user)
-
-
+  // respond with user info
   res.json({
     id: user.id,
     username: user.username,
-    updatedAt: user.updatedAt
-  })
-})
-
-
+    updatedAt: user.updatedAt,
+  });
+});
 
 router.get('/logout', (req, res) => {
-  //clear user data from session
+  // clear user data from session
   req.session.user = null;
 
-  //send success response
+  // send success response
   res.json({
-    success: 'Logged out successfully'
-  })
- })
+    success: 'Logged out successfully',
+  });
+});
 
- router.get('/current', (req, res) => {
-   const { user } = req.session;
-   if (user) {
-     res.json({
+router.get('/current', (req, res) => {
+  const { user } = req.session;
+  if (user) {
+    res.json({
       id: user.id,
       username: user.username,
-      updatedAt: user.updatedAt
-     })
-   } else {
-     res.status(401).json({
-       error: 'Not logged in',
-     })
-   }
- })
-
-
+      updatedAt: user.updatedAt,
+    });
+  } else {
+    res.status(401).json({
+      error: 'Not logged in',
+    });
+  }
+});
 
 module.exports = router;
